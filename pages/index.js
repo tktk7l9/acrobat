3a535819948cdc74b7586b87e4f86da2718a8d21
tktk7l9 <saitotakuya0719@gmail.com>
2021-02-07 12:00:18 +0900

    Pre-rendering and Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,43 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedGymsData } from '../lib/gyms'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allGymsData = getSortedGymsData()
+  return {
+    props: {
+      allGymsData
+    }
+  }
+}
+
+export default function Home({ allGymsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>アクロバットを練習できる場所を見つけるサイトです。</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.HeadingLg}>Gyms</h2>
+        <ul className={utilStyles.list}>
+          {allGymsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
