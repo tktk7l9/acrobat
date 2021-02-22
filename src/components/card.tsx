08d299c42a83b145422cc6678062ea28fa10cc51
tktk7l9 <saitotakuya0719@gmail.com>
2021-02-22 21:53:37 +0900

    refactoring

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,38 +1,56 @@
-import { GymsData } from '@src/types'
 import Link from 'next/link'
 
-export default function Card({ allGymsData }: { allGymsData: GymsData }) {
+export default function Card({ allGymsData }: { allGymsData: any }) {
 
   return (
     <>
       <ul className="grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6 list-none p-0 xl:mx-10 md:mx-3 sm:mx-1">
-        {allGymsData.map(( allGymData
+        {allGymsData.map((
+          {
+            id,
+            address,
+            building,
+            floor,
+            name,
+            branch,
+            tags,
+            siteImage
+          }:{
+              id: string,
+              address: string,
+              building: string,
+              floor: string,
+              name: string,
+              branch: string,
+              tags: string[],
+              siteImage: string
+            }
         ) => (
           <Link
             href="/gyms/[id]"
-            as={`/gyms/${allGymData.id}`}
+            as={`/gyms/${id}`}
           >
           <div className="rounded overflow-hidden shadow-lg my-2 hover:bg-gray-100">
               <img
                 className="w-full h-52"
-                src={allGymData.siteImage}
-                alt={allGymData.name}
+                src={siteImage}
+                alt={name}
               />
             <div className="px-6 py-4">
                   <div className="font-bold text-base mb-1">
-                    {allGymData.name} {allGymData.branch}
+                    {name} {branch}
                   </div>
                   <small className="text-gray-500">
-                  {allGymData.address} {allGymData.building}{allGymData.floor}
+                  {address} {building}{floor}
                     <br />
                     <div>
-                      {allGymData.tags.map((tag, i) => (
-                          <span
-                            className="text-sm  inline-block px-2 uppercase rounded text-white bg-green-400 uppercase last:mr-0 mr-1 mt-1"
-                            key={i}
-                          >
-                            {tag}
-                          </span>
+                      {tags.map((tag, i) => (
+                        <span
+                          className="text-sm  inline-block px-2 uppercase rounded text-white bg-green-400 uppercase last:mr-0 mr-1 mt-1"
+                          key={i}
+                            >
+                                {tag}
+                            </span>
                         ))}
                     </div>
                   </small>
