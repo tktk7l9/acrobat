b1f7d4478e6a560ecad967476c5dc5802905ed73
tktk7l9 <saitotakuya0719@gmail.com>
2021-03-02 22:04:35 +0900

    fix price notation

--- src/components/OpenInfo.tsx
+++ src/components/OpenInfo.tsx
@@ -7,6 +7,7 @@ export default function OpenInfo({ openInfo }: { openInfo: any }) {
         <li className="" key={weekday}>
           {Object.keys(openInfo[weekday]).map((index, i) => {
             const detail = openInfo[weekday]
+            const fee = detail[index].fee.toLocaleString()
             return (
               <div
                 className="flex justify-between flex-grow-0 py-1"
@@ -25,7 +26,7 @@ export default function OpenInfo({ openInfo }: { openInfo: any }) {
                   {detail[index].start} ~ {detail[index].end} ({detail[index].minutes}分)
                 </div>
                 <div className="order-last text-gray-600 px-2">
-                  {detail[index].fee}円
+                  {fee}円
                 </div>
               </div>
             )})}
